e45dc17469a93858f5c62f853bfa3888c58105a7
ekber97 <121440920+ekber97@users.noreply.github.com>
2023-02-13 23:47:31 +0400

    Update nermin.py

--- nermin.py
+++ nermin.py
@@ -1,7 +1,7 @@
 
 from komekci.aykhan import Nermin
 import base64
-from mesajlar.mesaj import salam, necesen, sagol, getdim, geldim, am, hi, hara, ds, nazli, sd, ny,ax, nrs, sev, gul1, gul2, brs, yuxu, haralisan, can, yas, nermin, ban
+from mesajlar.mesaj import salam, necesen, sagol, getdim, geldim, am, hi, hara, ds, nazli, bc, sd, ny,ax, nrs, sev, gul1, gul2, brs, yuxu, haralisan, can, yas, nermin, ban
 from mesajlar.bot import yeni_user, start
 from telethon import events, Button
 import random
@@ -66,7 +66,16 @@ async def yeni_mesaj(event: events.NewMessage.Event):
 async def yeni_mesaj(event: events.NewMessage.Event):
     await event.reply(f"{random.choice(getdim)}")
 
-
+@Nermin.on(events.NewMessage(pattern='(?i)Bacım+'))
+@Nermin.on(events.NewMessage(pattern='(?i)bacım+'))
+@Nermin.on(events.NewMessage(pattern='(?i)Bacı+'))
+@Nermin.on(events.NewMessage(pattern='(?i)bacı+'))
+@Nermin.on(events.NewMessage(pattern='(?i)Bacim+'))
+@Nermin.on(events.NewMessage(pattern='(?i)bacim+'))
+@Nermin.on(events.NewMessage(pattern='(?i)Baci+'))
+@Nermin.on(events.NewMessage(pattern='(?i)baci+'))
+async def yeni_mesaj(event: events.NewMessage.Event):
+    await event.reply(f"{random.choice(bc)}")
 
 @Nermin.on(events.NewMessage(pattern='(?i)Niyə yatmırsız+'))
 @Nermin.on(events.NewMessage(pattern='(?i)niyə yatmırsız+'))
